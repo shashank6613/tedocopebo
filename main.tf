59f12ad29a77b1cd362040639a483924cbc7606f
shank <shank@gmail.com>
2025-12-02 07:28:18 +0000

    added clone repo is user data

--- main.tf
+++ main.tf
@@ -123,6 +123,12 @@ resource "aws_instance" "app_host" {
     DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
     curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
+ 
+    # Switch to the correct user directory
+    cd /home/ec2-user
+    
+    # Clone the public repository
+    git clone https://github.com/shashank6613/tedocopebo.git
     
     echo "EC2 setup complete. Docker and Docker Compose installed."
     EOF
